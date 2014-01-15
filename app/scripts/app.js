3ecdc77bfdf73ecc569fa7c605ba29d7389614d6
Bálder Carraté <baldercm@gmail.com>
2014-01-16 00:47:01 +0100

    Yeoman AngularJS generator scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('hairyNemesisApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
